1230ad5e7710ba2879077643b585fc07ad4463d8
AG_peo <AG_peo@AG>
2025-11-10 22:55:51 +0200

    Add project files.

---
+++ LogicHere.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+
+namespace CodeHere
+{
+    public class LogicHere
+    {
+        public string expression { get; set; } = string.Empty;
+        public string result { get; set; } = string.Empty;
+        public void Append(char c)
+        {
+            expression += c;
+        }
+        public void Clear()
+        {
+            expression = string.Empty;
+            result = string.Empty;
+        }
+        public void Undo()
+        {
+            if (!string.IsNullOrEmpty(expression))
+            {
+                expression = expression.Substring(0, expression.Length - 1);
+            }
+        }
+        public void EvaluateExpression()
+        {
+            try
+            {
+                var dataTable = new DataTable();
+                var value = dataTable.Compute(expression, string.Empty);
+                expression = value.ToString();
+            }
+            catch (Exception ex)
+            {
+                expression = "Error: " + ex.Message;
+            }
+        }
+    }
+}
